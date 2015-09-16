7fecb9e3e7b13756be60bdc5929bcbfa5f483690
desi <desislava.manova@gmail.com>
2015-09-16 16:34:06 +0300

    added homework

---
+++ IntroToProgrammingCSharp/09.PrintASequence.cs
@@ -0,0 +1,24 @@
+﻿/*Problem 9. Print a Sequence
+
+Write a program that prints the first 10 members of the sequence: 2, -3, 4, -5, 6, -7, ...
+*/
+
+public class PrintASequence
+{
+    public static void Main();
+    {
+    for (int i = 2; i < 12; i++)
+        {
+         if (int % 2 == 0)
+        {
+        
+        Console.WriteLine(i);
+        }
+        else
+            {
+               Console.WriteLine("-" + i);
+            }
+        }
+    }
+}
+
