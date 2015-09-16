2d1abb5e0157de5e5fbc1e792bdd56410ee765d7
desi <desislava.manova@gmail.com>
2015-09-16 16:16:26 +0300

    added homework

---
+++ IntroToProgrammingCSharp-Homeworks/05.PrintYourName.cs
@@ -0,0 +1,13 @@
+﻿/*Problem 5. Print Your Name
+
+Modify the previous application to print your name.
+Ensure you have named the application well (e.g. “PrintMyName”).
+*/
+
+public class PrintYourName
+{
+  public static void Main()
+    {
+        System.Console.WriteLine("Print my name!");
+    }
+}
