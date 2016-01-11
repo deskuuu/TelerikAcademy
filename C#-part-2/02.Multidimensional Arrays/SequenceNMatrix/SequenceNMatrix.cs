cd89a941b97d28104379a000b93ff22ed7ffe7d7
desi <desislava.manova@gmail.com>
2016-01-11 21:37:22 +0200

    added homework

---
+++ C#-part-2/02.Multidimensional Arrays/SequenceNMatrix/SequenceNMatrix.cs
@@ -0,0 +1,20 @@
+﻿/*
+Problem 3. Sequence n matrix
+
+We are given a matrix of strings of size N x M. Sequences in the matrix we define as sets of several neighbour elements located on the same line, column or diagonal.
+Write a program that finds the longest sequence of equal strings in the matrix.
+ */
+using System;
+
+
+	class CreateAndPrintNMatrix
+    {
+    static void Main()
+        {
+        
+        
+
+        
+
+        }
+    }
