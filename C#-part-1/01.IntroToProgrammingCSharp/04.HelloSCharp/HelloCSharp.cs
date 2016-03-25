f52d7dfa344bf03ab0e87e99369b8901587b3001
Desislava Manova <deskuuu@users.noreply.github.com>
2016-03-25 21:41:42 +0300

    Update HelloCSharp.cs

--- C#-part-1/01.IntroToProgrammingCSharp/04.HelloSCharp/HelloCSharp.cs
+++ C#-part-1/01.IntroToProgrammingCSharp/04.HelloSCharp/HelloCSharp.cs
@@ -1,5 +1,5 @@
 ﻿/*Problem 4.Hello World
-Create, compile and run a “Hello C#” console application.
+Create, compile and run a “Hello, C#” console application.
 Ensure you have named the application well (e.g. “”HelloCSharp”).
 */
 
