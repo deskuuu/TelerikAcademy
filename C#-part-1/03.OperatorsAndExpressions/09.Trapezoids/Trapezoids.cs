854ba525e6ab55041c01d9bd6fcf890270193367
Desislava Manova <deskuuu@users.noreply.github.com>
2016-03-27 16:54:03 +0300

    Update Trapezoids.cs

--- C#-part-1/03.OperatorsAndExpressions/09.Trapezoids/Trapezoids.cs
+++ C#-part-1/03.OperatorsAndExpressions/09.Trapezoids/Trapezoids.cs
@@ -1,30 +1,22 @@
 ﻿/*Problem 9. Trapezoids
 
 Write an expression that calculates trapezoid's area by given sides a and b and height h.
-Examples:
-
-a	b	h	area
-5	7	12	72
-2	1	33	49.5
-8.5	4.3	2.7	17.28
-100	200	300	45000
-0.222	0.333	0.555	0.1540125
+The three values should be read from the console in the order shown below. All three value will be floating-point numbers.
 */
 using System;
+class Trapezoids
+{
+   static void Main()
+   {
+       Console.WriteLine("Write a small-side:");
+       double a = double.Parse(Console.ReadLine());
+       Console.WriteLine("Write a big-side:");
+       double b = double.Parse(Console.ReadLine());
+       Console.WriteLine("Write a height:");
+       double h = double.Parse(Console.ReadLine());
+       double trapArea = ((a + b) / 2) * h;
 
-    class Trapezoids
-    {
-        static void Main(string[] args)
-        {
-        Console.WriteLine("Write a small-side:");
-        double a = int.Parse(Console.ReadLine());
-        Console.WriteLine("Write a big-side:");
-        double b = int.Parse(Console.ReadLine());
-        Console.WriteLine("Write a height:");
-        double h = int.Parse(Console.ReadLine());
-        double trapArea = ((a + b) / 2) * h;
-
-        Console.WriteLine("{0} {1}", "The trapezoid area is ", trapArea);
+       Console.WriteLine("{0:f7}", trapArea);
     }
- }
+}
 
