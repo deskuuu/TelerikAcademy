9a56b235de7c986e67be5ef2787676eac4709384
Desislava Manova <deskuuu@users.noreply.github.com>
2016-03-27 14:48:49 +0300

    Update PrintASequence.cs

--- C#-part-1/01.IntroToProgrammingCSharp/09.PrintASequence/PrintASequence.cs
+++ C#-part-1/01.IntroToProgrammingCSharp/09.PrintASequence/PrintASequence.cs
@@ -4,10 +4,9 @@ Write a program that prints the first 10 members of the sequence: 2, -3, 4, -5, 
 */
 
 using System;
-
 class PrintASequence
 {
-    static void Main(string[] args)
+    static void Main()
     {
         for (int i = 2; i < 12; i++)
         {
