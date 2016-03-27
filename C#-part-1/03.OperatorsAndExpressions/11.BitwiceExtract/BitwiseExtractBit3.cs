fd5db7efacb196e65978651e82ac4b35f176b5a0
Desislava Manova <deskuuu@users.noreply.github.com>
2016-03-27 17:05:47 +0300

    Update BitwiseExtractBit3.cs

--- C#-part-1/03.OperatorsAndExpressions/11.BitwiceExtract/BitwiseExtractBit3.cs
+++ C#-part-1/03.OperatorsAndExpressions/11.BitwiceExtract/BitwiseExtractBit3.cs
@@ -1,29 +1,18 @@
 ﻿/*Problem 11. Bitwise: Extract Bit #3
 
-Using bitwise operators, write an expression for finding the value of the bit #3 of a given unsigned integer.
-The bits are counted from right to left, starting from bit #0.
-The result of the expression should be either 1 or 0.
-Examples:
-
-n	binary representation	bit #3
-5	00000000 00000101	0
-0	00000000 00000000	0
-15	00000000 00001111	1
-5343	00010100 11011111	1
-62241	11110011 00100001	0
+Using bitwise operators, write a program that uses an expression to find the value of the bit
+at index 3 of an unsigned integer read from the console.
+The bits are counted from right to left, starting from bit 0.
+The result of the expression should be either 1 or 0. Print it on the console.
 */
 using System;
-
-    class BitwiseExtractBit3
+class BitwiseExtractBit3
 {
-        static void Main(string[] args)
-        {
-
-        Console.WriteLine("Write a integer number:");
-        int number = int.Parse(Console.ReadLine());
-        int numberRightB = number >> 3 & 1;
-        Console.WriteLine("Tha value of bit #3 is:{0}", numberRightB);
-
+    static void Main()
+    {   
+       int number = int.Parse(Console.ReadLine());
+       int numberRightB = number >> 3 & 1;
+       Console.WriteLine(numberRightB);
     }
 }
 
