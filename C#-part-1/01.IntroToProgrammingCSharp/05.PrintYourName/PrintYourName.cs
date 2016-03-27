686c15be5b8c4907f49778d3b58009680c1bdabe
Desislava Manova <deskuuu@users.noreply.github.com>
2016-03-27 14:44:21 +0300

    Update PrintYourName.cs

--- C#-part-1/01.IntroToProgrammingCSharp/05.PrintYourName/PrintYourName.cs
+++ C#-part-1/01.IntroToProgrammingCSharp/05.PrintYourName/PrintYourName.cs
@@ -4,13 +4,10 @@ Modify the previous application to print your name.
 Ensure you have named the application well (e.g. “PrintMyName”).
 */
 using System;
-
 class PrintYourName
 {
-    static void Main(string[] args)
+    static void Main()
     {
-
         System.Console.WriteLine("Print my name!");
-
     }
 }
