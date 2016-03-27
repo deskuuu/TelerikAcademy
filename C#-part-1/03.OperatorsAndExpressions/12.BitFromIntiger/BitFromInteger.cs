bf6d9829b84dbcbfa2889ee6ec0b356ff156ac5a
Desislava Manova <deskuuu@users.noreply.github.com>
2016-03-27 17:09:19 +0300

    Update BitFromInteger.cs

--- C#-part-1/03.OperatorsAndExpressions/12.BitFromIntiger/BitFromInteger.cs
+++ C#-part-1/03.OperatorsAndExpressions/12.BitFromIntiger/BitFromInteger.cs
@@ -1,29 +1,19 @@
 ﻿/*Problem 12. Extract Bit from Integer
 
-Write an expression that extracts from given integer n the value of given bit at index p.
-Examples:
-
-n	   binary representation p	bit @ p
-5	   00000000 00000101	 2	1
-0	   00000000 00000000	 9	0
-15	   00000000 00001111	 1	1
-5343   00010100 11011111	 7	1
-62241  11110011 00100001	 11	0
+Write a program that reads from the console two integer numbers P and N and prints on the console the value of P's N-th bit.
 */
 using System;
-
-    class BitFromInteger
+class BitFromInteger
+{
+    static void Main()
     {
-        static void Main(string[] args)
-        {
-
-        Console.WriteLine("Write a integer number:");
-        int number = int.Parse(Console.ReadLine());
-        Console.WriteLine("Write a position of the bit:");
-        int positionOfBit = int.Parse(Console.ReadLine());
-        int bit  = number >> positionOfBit & 1;
-        Console.WriteLine("The value of bit {0} is {1}" , positionOfBit,bit);
-
+       Console.WriteLine("Write a integer number:");
+       int number = int.Parse(Console.ReadLine());
+       Console.WriteLine("Write a position of the bit:");
+       int positionOfBit = int.Parse(Console.ReadLine());
+       
+       int bit  = number >> positionOfBit & 1;
+       Console.WriteLine(bit)      
     }
- }
-
+    
+}
