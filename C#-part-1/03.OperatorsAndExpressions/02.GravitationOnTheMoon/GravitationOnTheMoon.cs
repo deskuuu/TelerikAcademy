0d47dd479025795f6efd27c7b4c4a0b98a4bf4fa
Desislava Manova <deskuuu@users.noreply.github.com>
2016-03-27 15:01:19 +0300

    Update GravitationOnTheMoon.cs

--- C#-part-1/03.OperatorsAndExpressions/02.GravitationOnTheMoon/GravitationOnTheMoon.cs
+++ C#-part-1/03.OperatorsAndExpressions/02.GravitationOnTheMoon/GravitationOnTheMoon.cs
@@ -1,31 +1,24 @@
 ﻿/*Problem 2. Gravitation on the Moon
 
-The gravitational field of the Moon is approximately 17% of that on the Earth.
-Write a program that calculates the weight of a man on the moon by a given weight on the Earth.
-Examples:
-
-weight	weight on the Moon
-86	    14.62
-74.6	12.682
-53.7	9.129
+Write a program that calculates the weight of a man on the moon by a given weight W(as a floating-point number) on the Earth.
 */
 using System;
-    class GravitationOnTheMoon
-    {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Please write your weight here -->...");
-        decimal a = decimal.Parse(Console.ReadLine());
-        if (a > 0)
-        {
-            Console.WriteLine("Your weight on the Moon is  " + (a * 17) / 100);
-        }
-        else
-        {
-            Console.WriteLine("Sorry, your result is incorrect. ");
-        }
-       
-        
-    }
+class GravitationOnTheMoon
+{
+     static void Main(string[] args)
+     {
+         Console.WriteLine("Please write your weight :");
+         float weight = float.Parse(Console.ReadLine());
+         float moonWeight = (weight * 17) / 100;
+          
+         if (weight > 0)
+         {
+         	Console.WriteLine("Your weight on the Moon is {0:0.000} ",moonWeight);
+         }
+         else
+         {
+            Console.WriteLine("Sorry, your result is incorrect.");
+         }
+     }
 }
 
