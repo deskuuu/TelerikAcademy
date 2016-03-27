d1722689b0ff6fa0ed692ec9d53cd2d32da018f3
Desislava Manova <deskuuu@users.noreply.github.com>
2016-03-27 14:44:39 +0300

    Update NumberSequence.cs

--- C#-part-1/01.IntroToProgrammingCSharp/06.NumberSequence/NumberSequence.cs
+++ C#-part-1/01.IntroToProgrammingCSharp/06.NumberSequence/NumberSequence.cs
@@ -4,10 +4,9 @@ Write a program to print the numbers 1, 101 and 1001, each at a separate line.
 Name the program correctly.
 */
 using System;
-
 class NumberSequence
 {
-    static void Main(string[] args)
+    static void Main()
     {
         System.Console.WriteLine("1\n101\n1001");
     }
