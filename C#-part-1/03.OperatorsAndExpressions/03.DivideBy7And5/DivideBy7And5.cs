2a3ac3758df3cfe96a89a88a61be2e99aa93d7ea
Desislava Manova <deskuuu@users.noreply.github.com>
2016-03-27 15:07:48 +0300

    Update DivideBy7And5.cs

--- C#-part-1/03.OperatorsAndExpressions/03.DivideBy7And5/DivideBy7And5.cs
+++ C#-part-1/03.OperatorsAndExpressions/03.DivideBy7And5/DivideBy7And5.cs
@@ -1,29 +1,23 @@
 ﻿/*Problem 3. Divide by 7 and 5
 
-Write a Boolean expression that checks for given integer if it can be divided (without remainder) by 7 and 5 at the same time.
-Examples:
-
-n	Divided by 7 and 5?
-3	false
-0	true
-5	false
-7	false
-35	true
-140	true
+Reads an integer number from the console.
+Stores in a variable if the number can be divided by 7 and 5 without remainder.
+Prints on the console "true NUMBER" if the number is divisible without remainder
+by 7 and 5. Otherwise prints "false NUMBER". In place of NUMBER print the value of the input number.
 */
 using System;
-    class DivideBy7And5
+class DivideBy7And5
     {
-        static void Main(string[] args)
+        static void Main()
         {
-
-        Console.WriteLine("Write an integer:");
-        int number = int.Parse(Console.ReadLine());
-        bool divideByFive = number % 5 == 0;
-        bool divideBySeven = number % 7 == 0;
-        bool divideBoth = divideByFive && divideBySeven;
-        Console.WriteLine("The number can be divided by seven and five - {0}", divideBoth);
-
+            int n = int.Parse(Console.ReadLine());
+         	bool isDividedByBoth = false;
+         	
+         	if (n % 7 == 0 && n % 5 == 0)
+         	{
+         		isDividedByBoth = true;
+         	}
+         	Console.WriteLine("{0} {1}", isDividedByBoth, n);
+        }
     }
-}
 
