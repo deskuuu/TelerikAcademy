f2c02070a147df172465c61a526a523ff252d4c8
Desislava Manova <deskuuu@users.noreply.github.com>
2016-03-27 16:22:43 +0300

    Update PointInACircle.cs

--- C#-part-1/03.OperatorsAndExpressions/07.PointInACircule/PointInACircle.cs
+++ C#-part-1/03.OperatorsAndExpressions/07.PointInACircule/PointInACircle.cs
@@ -1,34 +1,20 @@
 ﻿/*Problem 7. Point in a Circle
 
-Write an expression that checks if given point (x, y) is inside a circle K({0, 0}, 2).
-Examples:
-
-x	y	inside
-0	1	true
--2	0	true
--1	2	false
-1.5	-1	true
--1.5	-1.5	false
-100	-30	false
-0	0	true
-0.2	-0.8	true
-0.9	-1.93	false
-1	1.655	true
+Write a program that reads the coordinates of a point x and y and using an expression checks if given
+point (x, y) is inside a circle K({0, 0}, 2) - the center has coordinates (0, 0) and the circle has radius 2.
+The program should then print "yes DISTANCE" if the point is inside the circle or "no DISTANCE" if the point is outside the circle.
 */
 using System;
-
 class PointInACircule
+{
+    static void Main()
     {
-        static void Main(string[] args)
-        {
-        double x = 0.9;
-        double y = -1.93;
+        double x = double.Parse(Console.ReadLine());
+        double y = double.Parse(Console.ReadLine());
 
         //Formula is (x-k)^2 + (y-k)^2 = r^2
-
-        bool isInside = (x * x - y * y) <= 2 * 2;
-        Console.WriteLine("The point {0},{1} is {2} a circle", x,y, isInside ? "inside" : "outside");
-        
+        bool isInside = ((x * x + y * y) - 2 * (x - y + x)) <= 2 * 2;
+        Console.WriteLine(isInside);
     }
- }
+}
 
