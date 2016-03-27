8677b6c361ec743dc87daa05c18286de0fe4985f
Desislava Manova <deskuuu@users.noreply.github.com>
2016-03-27 14:43:56 +0300

    Update HelloCSharp.cs

--- C#-part-1/01.IntroToProgrammingCSharp/04.HelloSCharp/HelloCSharp.cs
+++ C#-part-1/01.IntroToProgrammingCSharp/04.HelloSCharp/HelloCSharp.cs
@@ -2,9 +2,7 @@
 Create, compile and run a “Hello, C#” console application.
 Ensure you have named the application well (e.g. “”HelloCSharp”).
 */
-
 using System;
-
 public class HelloCSharp
 {
     public static void Main()
