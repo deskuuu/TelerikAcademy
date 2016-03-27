076a5389b11439b1c01db461ebf2da1580d2d6b7
Desislava Manova <deskuuu@users.noreply.github.com>
2016-03-27 15:15:58 +0300

    Update Rectangles.cs

--- C#-part-1/03.OperatorsAndExpressions/04.Rectangeles/Rectangles.cs
+++ C#-part-1/03.OperatorsAndExpressions/04.Rectangeles/Rectangles.cs
@@ -1,28 +1,22 @@
 ﻿/*Problem 4. Rectangles
-
-Write an expression that calculates rectangle’s perimeter and area by given width and height.
-Examples:
-
-width	height	perimeter	area
-3	4	14	12
-2.5	3	11	7.5
-5	5	20	25
+Write an expression that calculates rectangle’s perimeter and
+area by given width and height. The width and height should be read from the console.
 */
 using System;
-
-   class Rectangles
+class IsPrime
+{
+    static void Main()
     {
-        static void Main(string[] args)
-        {
-        Console.WriteLine("Write a width :");
-        double width = int.Parse(Console.ReadLine());
-        Console.WriteLine("Write a height :");
-        double height = int.Parse(Console.ReadLine());
-
-        double rectangleArea = width * height;
-        double rectanglePerimeter = 2 * (width + height);
-
-        Console.WriteLine("{0}\n{1}\n{2}\n{3}", "The rectangle area is ",rectangleArea, "The rectangle perimeter is ",rectanglePerimeter);
+    	   Console.WriteLine("Write a width :");
+         double width = double.Parse(Console.ReadLine());
+         Console.WriteLine("Write a height :");
+         double height = double.Parse(Console.ReadLine());
+         
+         double rectangleArea = width * height;
+         double rectanglePerimeter = 2 * (width + height);
+         
+         Console.WriteLine("{0:f2} {1:f2}",rectanglePerimeter,rectangleArea);
     }
 }
+     
 
