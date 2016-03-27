81dfb2f13a1f8ace2ae3e653909bd75e2013e915
Desislava Manova <deskuuu@users.noreply.github.com>
2016-03-27 15:36:51 +0300

    Update ThirdDigitIs7.cs

--- C#-part-1/03.OperatorsAndExpressions/05.ThirdDigitIs7/ThirdDigitIs7.cs
+++ C#-part-1/03.OperatorsAndExpressions/05.ThirdDigitIs7/ThirdDigitIs7.cs
@@ -1,25 +1,15 @@
 ﻿/*Problem 5. Third Digit is 7?
 
 Write an expression that checks for given integer if its third digit from right-to-left is 7.
-Examples:
-
-n	Third digit 7?
-5	false
-701	true
-9703	true
-877	false
-777877	false
-9999799	true
 */
 using System;
-
-  class ThirdDigit
-    {
-        static void Main(string[] args)
-        {
-        Console.WriteLine("Write an integer: ");
-        int number = int.Parse(Console.ReadLine());
-        bool lastDigitIsSeven = (number / 100) % 10 == 7;
-        Console.WriteLine("{0} {1}", "The third digit is 7 from right-to-left -", lastDigitIsSeven);
-    }
+class ThirdDigit
+{
+     static void Main()
+     {
+         int number = int.Parse(Console.ReadLine());
+         int digit = (number / 100) % 10;
+         bool lastDigitIsSeven = digit == 7;
+         Console.WriteLine("{0} {1}",lastDigitIsSeven, digit);
+     }
 }
