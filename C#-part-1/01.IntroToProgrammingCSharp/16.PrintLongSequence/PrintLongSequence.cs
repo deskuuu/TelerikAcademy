73b70c5a79439bdcf1eedf5ef7ec1943d6c4b194
Desislava Manova <deskuuu@users.noreply.github.com>
2016-03-27 14:50:32 +0300

    Update PrintLongSequence.cs

--- C#-part-1/01.IntroToProgrammingCSharp/16.PrintLongSequence/PrintLongSequence.cs
+++ C#-part-1/01.IntroToProgrammingCSharp/16.PrintLongSequence/PrintLongSequence.cs
@@ -4,10 +4,9 @@ Write a program that prints the first 1001 members of the sequence: 2, -3, 4, -5
 You might need to learn how to use loops in C# (search in Internet).
 */
 using System;
-
 class PrintLongSequence
 {
-    static void Main(string[] args)
+    static void Main()
     {
         for (int i = 2; i <= 1001; i++)
         {
