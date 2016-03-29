1661b34f70b5df394feaf3002cd26d8ae85cd2ee
Desislava Manova <deskuuu@users.noreply.github.com>
2016-03-29 23:25:20 +0300

    Update IsoscelesTriangle.cs

--- C#-part-1/02.PrimitiveDateTypeAndVariables/08.IsoscelesTriangle/IsoscelesTriangle.cs
+++ C#-part-1/02.PrimitiveDateTypeAndVariables/08.IsoscelesTriangle/IsoscelesTriangle.cs
@@ -4,8 +4,7 @@ Write a program that prints an isosceles triangle of 9 copyright symbols ©, som
   © ©
  ©   ©
 © © © ©
-Note: The © symbol may be displayed incorrectly at the console so you may need to change the console character encoding to UTF-8 and assign a Unicode-friendly font in the console.
-Note: Under old versions of Windows the © symbol may still be displayed incorrectly, regardless of how much effort you put to fix it.*/using System;
+*/
 
 using System;
 using System.Collections.Generic;
@@ -17,7 +16,6 @@ class IsoscelesTriangle
         Console.WriteLine("  \u00A9 \u00A9  ");
         Console.WriteLine(" \u00A9   \u00A9 ");
         Console.WriteLine("\u00A9 \u00A9 \u00A9 \u00A9");
-
     }
 }
 
