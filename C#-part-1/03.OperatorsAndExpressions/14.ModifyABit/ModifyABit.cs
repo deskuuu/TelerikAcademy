c4b3159d90f9fe0886307f9eef9bcb4ebfe24913
Desislava Manova <deskuuu@users.noreply.github.com>
2016-03-30 01:57:26 +0300

    Update ModifyABit.cs

--- C#-part-1/03.OperatorsAndExpressions/14.ModifyABit/ModifyABit.cs
+++ C#-part-1/03.OperatorsAndExpressions/14.ModifyABit/ModifyABit.cs
@@ -1,20 +1,17 @@
-﻿/*Problem 14. Modify a Bit at Given Position
+﻿/*Problem 14. Modify Bit
 
-We are given an integer number N(read from the console), a bit value v(read from the console as well)
-(v = 0 or 1) and a position P(read from the console). Write a sequence of operators (a few lines of C# code) 
-that modifies N to hold the value v at the position P from the binary
-representation of N while preserving all other bits in N. Print the resulting number on the console.
+We are given an integer number N(read from the console), a bit value v(read from the console as well) (v = 0 or 1) 
+and a position P(read from the console). Write a sequence of operators (a few lines of C# code) that modifies N to hold 
+the value v at the position P 
+from the binary representation of N while preserving all other bits in N. Print the resulting number on the console.
 */
 using System;
 class ModifyABit
 {
    static void Main()
     {
-        Console.WriteLine("Enter the integer number:");
         int n = int.Parse(Console.ReadLine());
-        Console.WriteLine("Enter the position of the bit:");
         int p = int.Parse(Console.ReadLine());
-        Console.WriteLine("Enter the value of bit:");
         int v = int.Parse(Console.ReadLine());
 
         if (v == 0)
@@ -31,10 +28,5 @@ class ModifyABit
             Console.WriteLine();
             Console.WriteLine(result1);
             Console.WriteLine();
-        }
-        else
-        {
-            Console.WriteLine("Error!");
-         
         }
 }
