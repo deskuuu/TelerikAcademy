ac14da14f7754f94f119ba4cf575d374efdf1f31
Desislava Manova <deskuuu@users.noreply.github.com>
2016-04-04 22:12:58 +0300

    Update PntInsACirculeOutRect.cs

--- C#-part-1/03.OperatorsAndExpressions/10.PointInsideACircule/PntInsACirculeOutRect.cs
+++ C#-part-1/03.OperatorsAndExpressions/10.PointInsideACircule/PntInsACirculeOutRect.cs
@@ -1,22 +1,37 @@
 ﻿/*Problem 10. Point Inside a Circle & Outside of a Rectangle
 
-Write a program that reads a pair of coordinates x and y and uses an expression to checks for given point (x, y)
+Write a program that reads a pair of coordinates x and y and uses an expression to checks for given point (x, y) 
 if it is within the circle K({1, 1}, 1.5) and out of the rectangle R(top=1, left=-1, width=6, height=2).
 */
 using System;
-class PntInsACirculeOutRect
-{
-    static void Main()
-    {
-         double x = double.Parse(Console.ReadLine());
+class PointCircleRectangle
+	{
+		public static void Main()
+		{
+		 double x = double.Parse(Console.ReadLine());
          double y = double.Parse(Console.ReadLine());
-     
-         //Formula  for circle is (x-k)^2 + (y-k)^2 = r^2
-         bool inCircle = ((x - 1) * (x - 1) + (y - 1) * (y - 1)) <= 1.5 * 1.5;
-         bool outsideRectangle = (x <= -1 || x >= 6) || (y <= -1 || y >= 2);
-         bool inCircleOutsideRect = inCircle && outsideRectangle;
+          
+         bool isInCircle = (x - 1) * (x - 1) + (y - 1) * (y - 1) <= 1.5 * 1.5;
+         bool isInRectangle = (x >= 1 || x <= 6) && (y <= -1 || y >= 2);
+         bool isCircleOutsideRect = isInCircle && isInRectangle;
          
-         Console.WriteLine(inCircleOutsideRect ? "yes" : "no");
-   }
-}
+         if (isCircleOutsideRect)
+         {
+         	Console.WriteLine("inside circle outside rectangle");
+         }
+         else if (isInCircle && !isInRectangle)
+         {
+         	Console.WriteLine("inside circle inside rectangle");
+         }
+         else if (!isInCircle && isInRectangle)
+         {
+         	Console.WriteLine("outside circle outside rectangle");
+         }
+         else if (!isInCircle && !isInRectangle)
+         {
+         	Console.WriteLine("outside circle inside rectangle");
+         }
+		
+		}
+	}
 
