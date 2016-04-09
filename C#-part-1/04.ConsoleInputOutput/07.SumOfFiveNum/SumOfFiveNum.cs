22ebb051032d44d6718defab7ac8a68c3ba368d6
Desislava Manova <deskuuu@users.noreply.github.com>
2016-04-09 18:46:29 +0300

    Update SumOfFiveNum.cs

--- C#-part-1/04.ConsoleInputOutput/07.SumOfFiveNum/SumOfFiveNum.cs
+++ C#-part-1/04.ConsoleInputOutput/07.SumOfFiveNum/SumOfFiveNum.cs
@@ -1,29 +1,20 @@
 ﻿/*Problem 7. Sum of 5 Numbers
 
 Write a program that enters 5 numbers (given in a single line, separated by a space), calculates and prints their sum.
-Examples:
-
-numbers	             sum
-1 2 3 4 5	         15
-10 10 10 10 10	     50
-1.5 3.14 8.2 -1 0	11.84
 */
 using System;
-
-  class SumOfFiveNum
-    {
-        static void Main(string[] args)
-        {
-        Console.WriteLine("Enter a five numbers separated by space:");
-        string[] numbers = Console.ReadLine().Split(' ');
-        double sum = 0;
-
-        for (int i = 0; i < numbers.Length; i++)
-        {
-            sum += double.Parse(numbers[i]);
-        }
-        Console.WriteLine("The sum is : {0}", sum);
-       
-    }
+class SumOfFiveNum
+{
+    static void Main()
+   {
+      int a = int.Parse(Console.ReadLine());
+			int b = int.Parse(Console.ReadLine());
+			int c = int.Parse(Console.ReadLine());
+			int d = int.Parse(Console.ReadLine());
+			int e = int.Parse(Console.ReadLine());
+			
+			Console.WriteLine(a + b + c + d + e);
+   }
+  
 }
 
