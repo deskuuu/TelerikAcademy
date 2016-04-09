8ea94e62cc7f278ec5c0b6f9065eea0abe832f1b
Desislava Manova <deskuuu@users.noreply.github.com>
2016-04-09 18:24:10 +0300

    Update CompanyInformation.cs

--- C#-part-1/04.ConsoleInputOutput/02.CompanyInformation/CompanyInformation.cs
+++ C#-part-1/04.ConsoleInputOutput/02.CompanyInformation/CompanyInformation.cs
@@ -12,14 +12,18 @@ class CompanyInfo
           string companyName = Console.ReadLine();
           string companyAddress = Console.ReadLine();
           string companyPhone= Console.ReadLine();
-          string companyFax = string.Empty;
+          string companyFax = Console.ReadLine();
           string webSite = Console.ReadLine();
           string managerFirstName = Console.ReadLine();
           string managerLastName = Console.ReadLine();
           int managerAge = int.Parse(Console.ReadLine());
           string managerPhone = Console.ReadLine();
+          if (companyFax == string.Empty)
+          {
+            companyFax = "(no fax)";
+          }
 
-          Console.WriteLine("{0}\nAddress: {1}\nTel. {2}\nFax: (no fax){3}\nWeb site: {4}\nManager: {5} {6} (age: {7}, tel. {8})",companyName,companyAddress,companyPhone,companyFax,webSite,managerFirstName,managerLastName,managerAge,managerPhone);
+          Console.WriteLine("{0}\nAddress: {1}\nTel. {2:+359 ### ## ## ###}\nFax: {3}\nWeb site: {4}\nManager: {5} {6} (age: {7}, tel. {8:+359 # ### ###})",companyName,companyAddress,companyPhone,companyFax,webSite,managerFirstName,managerLastName,managerAge,managerPhone);
     }
 }
 
