eef5df58b94b5fc26c121ede3de6574746314c00
Desislava Manova <deskuuu@users.noreply.github.com>
2016-04-09 14:39:51 +0300

    Update CompanyInformation.cs

--- C#-part-1/04.ConsoleInputOutput/02.CompanyInformation/CompanyInformation.cs
+++ C#-part-1/04.ConsoleInputOutput/02.CompanyInformation/CompanyInformation.cs
@@ -5,35 +5,32 @@ Write a program that reads the information about a company and its manager and p
 */
 
 using System;
-
-    class Program
+class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+          Console.WriteLine("Enter a company name:");
+          string companyName = Console.ReadLine();
+          Console.WriteLine("Enter a company address:");
+          string companyAddress = Console.ReadLine();
+          Console.WriteLine("Enter a company phone number(10 digits):");
+          string companyNumber= Console.ReadLine();
+          Console.WriteLine("Enter a company fax number:");
+          string companyFax = null;
+          Console.WriteLine("Enter a web site:");
+          string webSite = Console.ReadLine();
 
-        Console.WriteLine("Enter a company name:");
-        string companyName = Console.ReadLine();
-        Console.WriteLine("Enter a company address:");
-        string companyAddress = Console.ReadLine();
-        Console.WriteLine("Enter a company phone number(10 digits):");
-        string companyNumber= Console.ReadLine();
-        Console.WriteLine("Enter a company fax number:");
-        string companyFax = null;
-        Console.WriteLine("Enter a web site:");
-        string webSite = Console.ReadLine();
+          Console.WriteLine("Enter a manager first name:");
+          string managerFirstName = Console.ReadLine();
+          Console.WriteLine("Enter a manager last name:");
+          string managerLastName = Console.ReadLine();
+          Console.WriteLine("Enter a manager age:");
+          byte managerAge = byte.Parse(Console.ReadLine());
+          Console.WriteLine("Enter a manager number phone:");
+          string managerNumber = Console.ReadLine();
 
-        Console.WriteLine("Enter a manager first name:");
-        string managerFirstName = Console.ReadLine();
-        Console.WriteLine("Enter a manager last name:");
-        string managerLastName = Console.ReadLine();
-        Console.WriteLine("Enter a manager age:");
-        byte managerAge = byte.Parse(Console.ReadLine());
-        Console.WriteLine("Enter a manager number phone:");
-        string managerNumber = Console.ReadLine();
-
-        Console.WriteLine(" Company name- {0}\n Company address- {1}\n Company number- {2}\n Company fax- {3}\n Web sait- {4}\n Manager first name- {5}\n Manager last name- {6}\n Manager age- {7}\n Manager number- {8}\n",companyName,companyAddress,companyNumber,companyFax,webSite,managerFirstName,managerLastName,managerAge,managerNumber);
-
-
+          Console.WriteLine(" Company name- {0}\n Company address- {1}\n Company number- {2}\n Company fax- {3}\n Web sait- {4}\n Manager first name- {5}\n Manager last name- {6}\n Manager age- {7}\n Manager number- {8}\n",companyName,companyAddress,companyNumber,companyFax,webSite,managerFirstName,managerLastName,managerAge,managerNumber);
+   
     }
 }
 
