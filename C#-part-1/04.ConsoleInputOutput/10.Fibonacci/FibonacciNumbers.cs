785954932466948bf7256ff748b11996405b4f3d
Desislava Manova <deskuuu@users.noreply.github.com>
2016-04-10 00:47:24 +0300

    Update FibonacciNumbers.cs

--- C#-part-1/04.ConsoleInputOutput/10.Fibonacci/FibonacciNumbers.cs
+++ C#-part-1/04.ConsoleInputOutput/10.Fibonacci/FibonacciNumbers.cs
@@ -1,37 +1,33 @@
 ﻿/*Problem 10. Fibonacci Numbers
 
-Write a program that reads a number n and prints on the console the first n members of the Fibonacci sequence (at a single line, separated by comma and space - ,) : 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, ….
-Note: You may need to learn how to use loops.
-
-Examples:
-
-n	comments
-1	0
-3	0 1 1
-10	0 1 1 2 3 5 8 13 21 34
+Write a program that reads a number n and prints on the console the first n members of the 
+Fibonacci sequence (at a single line, separated by comma and space - ,) : 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, ….
 */
-
 using System;
 using System.Numerics;
-
 class FibonacciNumbers
 {
     static void Main()
     {
-        Console.WriteLine("Enter lenght of Fib.Numbers: ");
         int n = int.Parse(Console.ReadLine());
         BigInteger firstNumber = 0;
         BigInteger secondNumber = 1;
         BigInteger temp = 0;
-        Console.Write(firstNumber + " " + secondNumber + " ");
+        int i =  2;
+        Console.Write(firstNumber + ", " + secondNumber + ", ");
 
-        for (int i = 2; i < n; i++)
+        for (i = 2; i < n - 1; i++)
         {
             temp = firstNumber + secondNumber;
-            Console.Write(temp + " ");
+            Console.Write(temp + ", ");
             firstNumber = secondNumber;
             secondNumber = temp;
-
+        }
+        i++;
+        temp = firstNumber + secondNumber;
+        if (i == n)
+        {
+        	 Console.Write(temp);
         }
         Console.WriteLine();
     }
