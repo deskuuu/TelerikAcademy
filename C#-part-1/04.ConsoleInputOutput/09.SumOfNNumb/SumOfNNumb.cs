3a9f7f5d91101688eea52d3a4cdc7f5e0af9a7e2
Desislava Manova <deskuuu@users.noreply.github.com>
2016-04-09 18:56:12 +0300

    Update SumOfNNumb.cs

--- C#-part-1/04.ConsoleInputOutput/09.SumOfNNumb/SumOfNNumb.cs
+++ C#-part-1/04.ConsoleInputOutput/09.SumOfNNumb/SumOfNNumb.cs
@@ -1,40 +1,21 @@
 ﻿/*Problem 9. Sum of n Numbers
 
-Write a program that enters a number n and after that enters more n numbers and calculates and prints their sum. Note: You may need to use a for-loop.
-Examples:
-
-numbers	sum
-3	    90
-20	
-60	
-10	
-5	    6.5
-2	
--1	
--0.5	
-4	
-2	
-1	   1
-1
+Write a program that enters a number n and after that enters more n numbers and calculates and prints their sum.
 */
 using System;
-
-    class SumOfNNumb
+class SumOfNNumb
+{
+    static void Main()
     {
-        static void Main(string[] args)
-        {
-        Console.WriteLine("Enter a numbers n (by space) to sum :");
-        string[] numbers = Console.ReadLine().Split(' ');
+        int n = int.Parse(Console.ReadLine());
         double sum = 0;
 
-        for (int i = 0; i < numbers.Length; i++)
+        for (int i = 0; i < n; i++)
         {
-            sum += double.Parse(numbers[i]);
-            Console.WriteLine("\n {0}",numbers[i]);
+            double number = double.Parse(Console.ReadLine());
+            sum += number;
         }
-        Console.WriteLine("The numbers are : {0}", numbers.Length);
-        Console.WriteLine("The sum is: {0}",sum);
-
-        }
+        Console.WriteLine(sum);
     }
+}
 
