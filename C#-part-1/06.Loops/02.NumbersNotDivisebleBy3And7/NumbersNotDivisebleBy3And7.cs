9053cca0fddf1ca37da166df79e91d48b01e75ac
Desislava Manova <deskuuu@users.noreply.github.com>
2016-04-11 16:06:56 +0300

    Update NumbersNotDivisebleBy3And7.cs

--- C#-part-1/06.Loops/02.NumbersNotDivisebleBy3And7/NumbersNotDivisebleBy3And7.cs
+++ C#-part-1/06.Loops/02.NumbersNotDivisebleBy3And7/NumbersNotDivisebleBy3And7.cs
@@ -1,29 +1,21 @@
 ﻿/*Problem 2. Numbers Not Divisible by 3 and 7
 
-Write a program that enters from the console a positive integer n and prints all the numbers from 1 to n not divisible by 3 or 7, on a single line, separated by a space.
-Examples:
-
-n	output
-3	1 2
-10	1 2 4 5 8 10
+Write a program that enters from the console a positive integer n and prints all the numbers from 1 to n
+not divisible by 3 or 7, on a single line, separated by a space.
 */
-
 using System;
-
-    class NumbersNotDivisebleBy3And7
+class NumbersNotDivisebleBy3And7
+{
+    static void Main()
     {
-        static void Main()
-        {
-        Console.WriteLine("Enter a positive integer n:");
         int n = int.Parse(Console.ReadLine());
             
-          for (int i = 0; i <= n; i++)
-          {
+        for (int i = 1; i <= n; i++)
+        {
             if (i % 3 != 0 && (i % 7 != 0))
             {
-              Console.WriteLine(i);
+               Console.WriteLine(i);
             }         
-          }
-         }      
-    }
-
+        }
+    }      
+}
