734df44563ea1a143acfc1663f2a14c1f01cf77e
Desislava Manova <deskuuu@users.noreply.github.com>
2016-04-11 15:21:35 +0300

    Update BonusScore.cs

--- C#-part-1/05.ConditionalStatements/02.BonosScore/BonusScore.cs
+++ C#-part-1/05.ConditionalStatements/02.BonosScore/BonusScore.cs
@@ -1,32 +1,23 @@
 ﻿/*Problem 2. Bonus Score
 
 Write a program that applies bonus score to given score in the range [1…9] by the following rules:
+
 If the score is between 1 and 3, the program multiplies it by 10.
 If the score is between 4 and 6, the program multiplies it by 100.
 If the score is between 7 and 9, the program multiplies it by 1000.
-If the score is 0 or more than 9, the program prints “invalid score”.
-Examples:
-
-score	result
-2	20
-4	400
-9	9000
--1	invalid score
-10	invalid score
+If the score is less than 0 or more than 9, the program prints "invalid score".
 */
 using System;
-
-  class BonusScore
+class BonusScore
 {
-        static void Main(string[] args)
-        {
-        Console.WriteLine("Enter a score(1-9):");
+    static void Main()
+    {
         int score = int.Parse(Console.ReadLine());
         int bonus = 0;
 
         if (score < 0)
         {
-            Console.WriteLine("Invalid score");
+            Console.WriteLine("invalid score");
         }
         else  if ((score >= 1) && (score <= 3))
         {
@@ -43,7 +34,6 @@ using System;
             bonus = score * 1000;
             Console.WriteLine(bonus);
         }
-        
     }
 }
 
