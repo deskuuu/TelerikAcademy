7c8fad2d934b185f0a7eff13c80a3664d2f165e6
Desislava Manova <deskuuu@users.noreply.github.com>
2016-04-11 18:38:33 +0300

    Update CalculateCombinatorics.cs

--- C#-part-1/06.Loops/07.CalculateCombinatorics/CalculateCombinatorics.cs
+++ C#-part-1/06.Loops/07.CalculateCombinatorics/CalculateCombinatorics.cs
@@ -1,47 +1,31 @@
 ﻿/*Problem 7. Calculate N! / (K! * (N-K)!)
 
-In combinatorics, the number of ways to choose k different members out of a group of n different elements
-(also known as the number of combinations) is calculated by the following formula: formula For example,
-there are 2598960 ways to withdraw 5 cards out of a standard deck of 52 cards.
 Your task is to write a program that calculates n! / (k! * (n-k)!) for given n and k (1 < k < n < 100). Try to use only two loops.
 */
-
 using System;
-  class CalculateCombinatorics
+using System.Numerics;
+class CalculateCombinatorics
+{
+  static BigInteger SumFactorial(int n)
+	{
+    BigInteger result = 1;
+    while (true)
     {
-        static void Main()
-        {
-
-        Console.WriteLine("Enter a number n:");
-        int n = int.Parse(Console.ReadLine());
-        Console.WriteLine("Enter a number k:");
-        int k = int.Parse(Console.ReadLine());
-        int nFactorial = 1;
-        int kFactorial = 1;
-        int counter = 1;
-        int sum = 1;
-
-        while (counter <= n)
-        {
-            nFactorial *= counter;
-
-            if (counter <= k)
-            {
-                kFactorial *= counter;
-            }
-            counter++;
-        }
-
-        counter = 1;
-        while (counter <= (n - k))
-        {
-            sum *= counter;
-            counter++;
-        }
-
-        sum = (nFactorial / (kFactorial * sum));
-
-        Console.WriteLine(sum);
+      if (n == 1)
+      {
+        break;
+      }
+      result *= n;
+      n--;
+    }
+    return result;
+	}
+    static void Main()
+    {
+    	int n = int.Parse(Console.ReadLine());
+      int k = int.Parse(Console.ReadLine());
+    	
+      BigInteger sum = SumFactorial(n) / (SumFactorial(k) * SumFactorial(n-k));
+      Console.WriteLine(sum);
     }
 }
-       
