fe1c93cee082ea35981b3779e59727a90dfaf93c
Desislava Manova <deskuuu@users.noreply.github.com>
2016-04-11 15:41:08 +0300

    Update TheBiggestOfFiveNum.cs

--- C#-part-1/05.ConditionalStatements/06.TheBiggestOfFiveNum/TheBiggestOfFiveNum.cs
+++ C#-part-1/05.ConditionalStatements/06.TheBiggestOfFiveNum/TheBiggestOfFiveNum.cs
@@ -1,76 +1,40 @@
 ﻿/*Problem 6. The Biggest of Five Numbers
 
 Write a program that finds the biggest of five numbers by using only five if statements.
-Examples:
-
-a	b	c	d	e	biggest
-5	2	2	4	1	5
--2	-22	1	0	0	1
--2	4	3	2	0	4
-0	-2.5	0	5	5	5
--3	-0.5	-1.1	-2	-0.1	-0.1
 */
 
 using System;
-
-    class TheBiggestOfFiveNum
+class TheBiggestOfFiveNum
     {
-        static void Main(string[] args)
+        static void Main()
         {
-        Console.WriteLine("Enter a first number :");
-        double a = double.Parse(Console.ReadLine());
-        Console.WriteLine("Enter a second number :");
-        double b = double.Parse(Console.ReadLine());
-        Console.WriteLine("Enter a third number :");
-        double c = double.Parse(Console.ReadLine());
-        Console.WriteLine("Enter a forth number :");
-        double d = double.Parse(Console.ReadLine());
-        Console.WriteLine("Enter a fifth number :");
-        double e = double.Parse(Console.ReadLine());
-        double biggest = 0;
-
-        if (a > b)
-        {
-            biggest = a;
-
-            if (c > biggest)
-            {
-                biggest = c;
-
-            }
-
-            if (d > biggest)
-            {
-                biggest = d;
-            }
-
-            if (e > biggest)
-            {
-                biggest = e;
-            }
+           double a = double.Parse(Console.ReadLine());
+           double b = double.Parse(Console.ReadLine());
+           double c = double.Parse(Console.ReadLine());
+           double d = double.Parse(Console.ReadLine());
+           double e = double.Parse(Console.ReadLine());
+           double biggest = 0;
+           
+           if (a > b && a > c && a > d && a > e)
+           {
+               biggest = a;
+           }
+           else if (b > a && b > c && b > d && b > e)
+           {
+               biggest = b;
+           }
+           else if (c > b && c > a && c > d && c > e)
+           {
+               biggest = c;
+           }
+           else if (d > b && d > c && d > a && d > e)
+           {
+               biggest = d;
+           }
+           else
+           {
+               biggest = e;
+           }
+           Console.WriteLine(biggest);
         }
-        else
-        {
-            biggest = b;
-
-            if (c > biggest)
-            {
-                biggest = c;
-            }
-
-            if (d > biggest)
-            {
-                biggest = d;
-            }
-
-            if (e > biggest)
-            {
-                biggest = e;
-            }
-        }
-
-        Console.WriteLine("The biggest is  {0}", biggest);
-    }
 }
-    
-
