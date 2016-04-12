42d533012cb91a3b234d9a3f765f5bfd4415570a
Desislava Manova <deskuuu@users.noreply.github.com>
2016-04-13 01:10:06 +0300

    Update MultiplicationSign.cs

--- C#-part-1/05.ConditionalStatements/04.MultiplicationSign/MultiplicationSign.cs
+++ C#-part-1/05.ConditionalStatements/04.MultiplicationSign/MultiplicationSign.cs
@@ -5,24 +5,24 @@ Write a program that shows the sign (+, - or 0) of the product of three real num
 using System;
 class MultiplicationSign
 {
-        static void Main()
-        {
-           double a = double.Parse(Console.ReadLine());
-           double b = double.Parse(Console.ReadLine());
-           double c = double.Parse(Console.ReadLine());
+    static void Main()
+    {
+        double a = double.Parse(Console.ReadLine());
+        double b = double.Parse(Console.ReadLine());
+        double c = double.Parse(Console.ReadLine());
 
-           if (a > 0 && b > 0 && c > 0)
-           {
-              Console.WriteLine("+");
-           }  
-           else if (a == 0 || b == 0 || c == 0)
-           {
-              Console.WriteLine("0");
-           }
-           else if (a < 0 ^ b < 0 ^ c < 0)
-           {
-              Console.WriteLine("-");
-           }
+        if ((a > 0 && b > 0 && c > 0) || (a < 0 && b > 0 && c < 0) || (a < 0 && b < 0 && c > 0) || (a > 0 && b < 0 && c < 0))
+        {
+            Console.WriteLine("+");
         }
+        else if (a == 0 || b == 0 || c == 0)
+        {
+            Console.WriteLine("0");
+        }
+        else if (a < 0 ^ b < 0 ^ c < 0)
+        {
+            Console.WriteLine("-");
+        }
+    }
 }
 
