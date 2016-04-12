ec8a0eced118e0d13ebb85d058316422bd963556
Desislava Manova <deskuuu@users.noreply.github.com>
2016-04-12 23:58:17 +0300

    Update TheBiggestOfThreeNum.cs

--- C#-part-1/05.ConditionalStatements/05.TheBiggestOfThreeNum/TheBiggestOfThreeNum.cs
+++ C#-part-1/05.ConditionalStatements/05.TheBiggestOfThreeNum/TheBiggestOfThreeNum.cs
@@ -9,7 +9,7 @@ class TheBiggestOfThreeNum
     static void Main()
     {
         double a = double.Parse(Console.ReadLine());
-        double b = double.Parse(Console.ReadLine())''
+        double b = double.Parse(Console.ReadLine());
         double c = double.Parse(Console.ReadLine());
 
         if (a > b || a > c)
