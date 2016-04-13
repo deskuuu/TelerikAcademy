862da47fb5ba486eff1f56542acfe554ed78a578
Desislava Manova <deskuuu@users.noreply.github.com>
2016-04-13 21:08:46 +0300

    Update PrintLongSequence.cs

--- C#-part-1/01.IntroToProgrammingCSharp/16.PrintLongSequence/PrintLongSequence.cs
+++ C#-part-1/01.IntroToProgrammingCSharp/16.PrintLongSequence/PrintLongSequence.cs
@@ -1,7 +1,6 @@
 ﻿/*Problem 16.* Print Long Sequence
 
 Write a program that prints the first 1001 members of the sequence: 2, -3, 4, -5, 6, -7, …
-You might need to learn how to use loops in C# (search in Internet).
 */
 using System;
 class PrintLongSequence
@@ -16,7 +15,7 @@ class PrintLongSequence
             }
             else
             {
-                Console.WriteLine("-" + i);
+                Console.WriteLine(-i);
             }
         }
 
