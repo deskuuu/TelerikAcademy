c36b581f83c8e9d704f0a62a73dee2c2afc2443f
Desislava Manova <deskuuu@users.noreply.github.com>
2016-04-13 21:02:42 +0300

    Update SquereRoot.cs

--- C#-part-1/01.IntroToProgrammingCSharp/08.SquereRoot/SquereRoot.cs
+++ C#-part-1/01.IntroToProgrammingCSharp/08.SquereRoot/SquereRoot.cs
@@ -1,13 +1,12 @@
 ﻿/*Problem 8. Square Root
 
 Create a console application that calculates and prints the square root of the number 12345.
-Find in Internet “how to calculate square root in C#”.
 */
 using System;
 class SquereRoot
 {
     static void Main()
     {
-        System.Console.WriteLine(Math.Sqrt(12345));
+        Console.WriteLine(Math.Sqrt(12345));
     }
 }
