4d5c9a04a8deb2da7133783c29381377570a0690
Desislava Manova <deskuuu@users.noreply.github.com>
2016-04-13 23:47:40 +0300

    Update MatrixOfNumbers.cs

--- C#-part-1/06.Loops/09.MatrixOfNumbers/MatrixOfNumbers.cs
+++ C#-part-1/06.Loops/09.MatrixOfNumbers/MatrixOfNumbers.cs
@@ -1,37 +1,27 @@
-﻿/*Problem 9. Matrix of Numbers
+/*Problem 9. Matrix of Numbers
 
-Write a program that reads from the console a positive integer number n (1 ≤ n ≤ 20) and prints a matrix like in the examples below. Use two nested loops.
+Write a program that reads from the console a positive integer number n (1 ≤ n ≤ 20) and prints a matrix.
 */
 
 using System;
-
-    class MatrixOfNumbers
+class MatrixOfNumbers
+{
+    static void Main()
     {
-        static void Main()
-        {
-
-        Console.WriteLine("Enter a positive integer (1 <= n <= 20)");
         int n = int.Parse(Console.ReadLine());
         int number = 1;
 
-        if (n <= 20)
+        for (int cols = 1; cols <= n; cols++)
         {
-            for (int cols = 1; cols <= n; cols++)
+            number = cols;
+            for (int rows = 1; rows <= n; rows++)
             {
-                number = cols;
-                for (int rows = 1; rows <= n; rows++)
-                {
-                    Console.Write("{0 }", number);
-                    number++;
-                }
-                Console.WriteLine();
+                Console.Write("{0 }", number);
+                number++;
             }
+            Console.WriteLine();
         }
-        else
-        {
-            Console.WriteLine("Your input is wrong, try again!");
-        }
-
+    
     }
-    }
+}
 
