3ea7d94a9f9e986c024feea8cf95eb2e97c960ba
Desislava Manova <deskuuu@users.noreply.github.com>
2016-04-15 09:29:57 +0300

    Update MinMaxSum.cs

--- C#-part-1/06.Loops/03.MinMaxSum/MinMaxSum.cs
+++ C#-part-1/06.Loops/03.MinMaxSum/MinMaxSum.cs
@@ -32,9 +32,9 @@ class MinMaxSumAvg
            avg = (double)sum / n;
         }
         
-        Console.WriteLine("min={0}", min);
-        Console.WriteLine("max={0}", max);
-        Console.WriteLine("sum={0}", sum);
+        Console.WriteLine("min={0:F2}", min);
+        Console.WriteLine("max={0:F2}", max);
+        Console.WriteLine("sum={0:F2}", sum);
         Console.WriteLine("avg={0:F2}", avg);
     }
 }
