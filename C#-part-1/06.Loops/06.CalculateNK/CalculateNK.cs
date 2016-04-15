c351e80e986d668335ef3a84a15b1dd8883d1c14
Desislava Manova <deskuuu@users.noreply.github.com>
2016-04-15 12:49:14 +0300

    Update CalculateNK.cs

--- C#-part-1/06.Loops/06.CalculateNK/CalculateNK.cs
+++ C#-part-1/06.Loops/06.CalculateNK/CalculateNK.cs
@@ -3,29 +3,29 @@
 Write a program that calculates n! / k! for given n and k (1 < k < n < 100).
 */
 using System;
+using System.Numerics;
 class CalculateNK
 {
+    static BigInteger SumFactorial(int n)
+    {
+        BigInteger result = 1;
+        while (true)
+        {
+            if (n == 1)
+            {
+                break;
+            }
+            result *= n;
+            n--;
+        }
+        return result;
+    }
     static void Main()
     {
         int n = int.Parse(Console.ReadLine());
         int k = int.Parse(Console.ReadLine());
-        
-        int nFactorial = 1;
-        int kFactorial = 1;
-        int counter = 1;
-        int div = 1;
 
-        while (counter <= n)
-        {
-            nFactorial *= counter;
-            
-            if (counter <= k)
-            {
-                kFactorial *= counter;
-            }
-            div = nFactorial / kFactorial;
-            counter++;         
-        }
-        Console.WriteLine(div);
+        BigInteger sum = SumFactorial(n) / SumFactorial(k);
+        Console.Write(sum);
     }
 }
