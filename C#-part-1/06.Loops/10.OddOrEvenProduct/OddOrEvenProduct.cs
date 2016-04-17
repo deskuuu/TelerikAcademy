c015db1456d7defd8ce579119588f2603070f434
Desislava Manova <deskuuu@users.noreply.github.com>
2016-04-17 23:00:13 +0300

    Update OddOrEvenProduct.cs

--- C#-part-1/06.Loops/10.OddOrEvenProduct/OddOrEvenProduct.cs
+++ C#-part-1/06.Loops/10.OddOrEvenProduct/OddOrEvenProduct.cs
@@ -2,33 +2,30 @@
 
 You are given n integers (given in a single line, separated by a space).
 Write a program that checks whether the product of the odd elements is equal to the product of the even elements.
-Elements are counted from 1 to n, so the first element is odd, the second is even, etc.
 */
-
 using System;
-
-    class OddOrEvenProduct
+class OddOrEvenProduc
+{
+    static void Main()
     {
-        static void Main()
-        {
-        Console.WriteLine("Enter numbers separated by space:");
-        string[] input = Console.ReadLine().Trim().Split(' ');
-        decimal oddP = 1;
-        decimal evenP = 1;
+        int n = int.Parse(Console.ReadLine());
+        string input = Console.ReadLine();
+        var arrayOfNumbers = input.Split(' ');
+        long oddSum = 1;
+        long evenSum = 1;
 
-        foreach (string num in input)
+        for (int i = 1; i < arrayOfNumbers.Length + 1; i++)
         {
-            if (int.Parse(num) % 2 == 0)
+            if (i % 2 != 0)
             {
-                oddP *= int.Parse(num);
+                oddSum *= int.Parse(arrayOfNumbers[i - 1]);
             }
             else
             {
-                evenP *= int.Parse(num);
+                evenSum *= int.Parse(arrayOfNumbers[i - 1]);
             }
         }
-        string isEqual = (oddP == evenP) ? "is equal" : "isn`t equal";
-        Console.WriteLine("The product of the even elements {0} {1} to the product of the odd elements {2}.", evenP, isEqual, oddP);
-        }
+        bool isEqual = (oddSum == evenSum);
+        Console.WriteLine(isEqual ? "yes {0}" : "no {0} {1}", oddSum, evenSum);
     }
-
+}
