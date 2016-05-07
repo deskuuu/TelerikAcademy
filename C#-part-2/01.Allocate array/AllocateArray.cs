8d74426ee1cbdf74707c387b69d00086d8aaa21a
desi <desislava.manova@gmail.com>
2016-05-07 11:30:02 +0300

    added first homework of arrays

---
+++ C#-part-2/01.Allocate array/AllocateArray.cs
@@ -0,0 +1,20 @@
+﻿//Write a program that allocates array of N integers, initializes each element by its index multiplied by 5 and the prints the obtained array on the console.
+namespace Allocate_array
+{
+    using System;
+
+    class AllocateArray
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            var arr = new int[n];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = i * 5;
+                Console.WriteLine(arr[i]);
+            }
+        }
+    }
+}
---
+++ C#-part-2/19.Permutations of set/PermutationsOfSet.cs
@@ -0,0 +1,23 @@
+﻿//Write a program that reads a number N and generates and prints all the permutations of the numbers[1 … N].
+namespace _19.Permutations_of_set
+{
+    using System;
+    using System.Collections.Generic;
+
+    class PermutationsOfSet
+    {
+        static void Main()
+        {
+            int input = int.Parse(Console.ReadLine());
+
+            var arr = new List<int>(input);
+
+            for (int i = 0; i <= input - 1; i++)
+            {
+                arr.Add(i + 1);
+            }
+
+            //TODO: think about logic!
+        }
+    }
+}
